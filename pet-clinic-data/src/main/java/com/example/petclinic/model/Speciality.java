fecb470fcb9147f0d97cbf3970aa096b26c8f834
Stanislav Machel <stanislav.machel@gmail.com>
2019-01-15 20:32:53 +0200

    Create Vet Speciality Entity and assign it to Vet. Closes #51.

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.example.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String desctiption;
+
+    public String getDesctiption() {
+        return desctiption;
+    }
+
+    public void setDesctiption(String desctiption) {
+        this.desctiption = desctiption;
+    }
+}
