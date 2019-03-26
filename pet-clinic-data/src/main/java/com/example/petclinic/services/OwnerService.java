fe12a00fb6bf6dcd95e700cf4173f26aed8b5734
Stanislav Machel <stanislav.machel@gmail.com>
2019-03-26 14:50:41 +0200

    Impl find owners

--- pet-clinic-data/src/main/java/com/example/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/OwnerService.java
@@ -2,10 +2,11 @@ package com.example.petclinic.services;
 
 import com.example.petclinic.model.Owner;
 
+import java.util.List;
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
+    List<Owner> findAllByLastNameLike(String lastName);
 }
